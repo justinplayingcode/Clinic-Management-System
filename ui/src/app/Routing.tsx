db744267678e5df6747a6d84d87e7050775ce7f5
justin.pham <phamduythang00@gmail.com>
2023-10-30 20:15:44 +0700

    add landingpage

--- ui/src/app/Routing.tsx
+++ ui/src/app/Routing.tsx
@@ -1,14 +1,16 @@
-import { Navigate, Route, HashRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import UniformLayout from "./structure";
 import { Appointment, Histories, Overview, Schedule } from "./page";
 import { routerString } from "./model/router";
+import LandingPage from "./page/LandingPage";
 
 function Routing() {
   return (
     <Router basename="/">
       <Routes>
-        <Route path={`${routerString.login}`} element={<>login</>} />
-        <Route path="/" element={<Navigate to={`${routerString.home}`} replace />} />
+        {/* <Route path={`${routerString.login}`} element={<LandingPage/>} /> */}
+        <Route path={`/`} element={<LandingPage/>} />
+        {/* <Route path="/" element={<Navigate to={`${routerString.home}`} replace />} /> */}
         <Route path={`${routerString.home}`} element={<UniformLayout page={<Overview/>}/>} />
         <Route path={`${routerString.histories}`} element={<UniformLayout page={<Histories/>}/>} />
         <Route path={`${routerString.appointment}`} element={<UniformLayout page={<Appointment/>}/>} />
