48228b3bc559f7c526e81cb2fc117627af98323a
dangtienhoang37 <dangtienhoang37@gmail.com>
2023-11-18 16:46:37 +0700

    del history

--- .history/server/src/module/typeAppointment/typeAppointment.repository_20231110032214.ts
+++
@@ -1,3 +0,0 @@
-import BaseRepository from "../common/common.repository";
-import { typeAppointmentModel } from "./typeAppointment.model";
-export default class typeAppointmentRepository extends BaseRepository<typeAppointmentModel> {}
--- .history/server/src/module/typeAppointment/typeAppointment.repository_20231118162109.ts
+++
@@ -1,7 +0,0 @@
-import BaseRepository from "../common/common.repository";
-import { typeAppointmentModel } from "./typeAppointment.model";
-export default class typeAppointmentRepository extends BaseRepository<typeAppointmentModel> {
-  public getAll = async () => {
-    return await this.model.find();
-  }
-}
