2d4766503380638c54ace216063199aa356e8852
justin.pham <phamduythang00@gmail.com>
2023-11-25 16:36:02 +0700

    picker common

--- ui/src/app/page/Histories/index.tsx
+++ ui/src/app/page/Histories/index.tsx
@@ -1,7 +1,27 @@
+import Picker from "../components/picker";
+
 function Histories() {
   return (
     <div>
-      Histories
+      <Picker
+        label={"Chuẩn đoán bệnh: "}
+        onChangeCallBack={(value) => {console.log(value)}}
+        value={[]}
+        integrateItems={() => Promise.resolve()}
+        mappingValues={(datas) => {
+          const values = datas.map((e: any) => {
+            return {
+              displayName: e.name,
+              text: e.name,
+              secondaryText: e.designation,
+              id: e._id,
+              usage: e.usage
+            }
+          })
+          return values
+        }}
+        placeholder={"Nhập tên bệnh"}
+      />
     </div>
   );
 }
